089cb92709839d2497fb73ca50624417e8e4ed56
Akshit <akshitv2004@gmail.com>
2025-04-20 13:28:20 +0530

    User model,route,controller,middleware written

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,149 @@
+import bcrypt from "bcryptjs";
+import { db } from "../libs/db.js";
+import { UserRole } from "../generated/prisma/index.js";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const existingUser = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        error: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await db.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+        role: UserRole.USER,
+      },
+    });
+
+    const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    //Setting the cookie
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: 1000 * 60 * 60 * 24 * 7, //7 days
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully!",
+      user: {
+        id: newUser.id,
+        name: newUser.name,
+        email: newUser.email,
+        role: newUser.role,
+        image: newUser.image,
+      },
+    });
+  } catch (error) {
+    console.error("Error creating user: ", error);
+    res.status(500).json({
+      success: false,
+      error: "Error creating user",
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        error: "User not found",
+      });
+    }
+
+    const isMatching = await bcrypt.compare(password, user.password);
+
+    if (!isMatching) {
+      res.status(401).json({
+        success: false,
+        error: "Invalid credentials",
+      });
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: 1000 * 60 * 60 * 24 * 7, //7 days
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "User logged-in successfully!",
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        image: user.image,
+      },
+    });
+  } catch (error) {
+    console.error("Error logging-in user: ", error);
+    res.status(500).json({
+      success: false,
+      error: "Error loggin-in user",
+    });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("jwt", {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "User logged-out successfully",
+    });
+  } catch (error) {}
+};
+
+export const check = async (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      message: "User authenticated successfully",
+      user: req.user,
+    });
+  } catch (error) {
+    console.error("Error checking user: ", error);
+    res.status(500).json({ message: "Error checking user" });
+  }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,26 @@
 import express  from "express";
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config()
 
 
 const app=express();
+
+// app.use in Express.js is used to define middleware functions that are executed for every request to the app, or for specific routes.
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.get("/",(req,res)=>{
+    res.send("Hey guys! Welcome to Codentia!!")
+    
+})
+
+app.use("/api/v1/auth",authRoutes)
+
+
 
 app.listen(process.env.PORT,()=>{
     console.log("Server is running on port 8080")
